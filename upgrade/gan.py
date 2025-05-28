3f13c2862650ca4fc865c02afe34b2ee3fb1f865
Conspiciens <fernandezjohann08@gmail.com>
2025-05-28 13:49:31 -0700

    A more simple gan to start

--- upgrade/gan.py
+++ upgrade/gan.py
@@ -3,75 +3,30 @@ import torch
 import torch.nn as nn
 
 
-# VGG 16 Model 
+# VGG 16 Model - https://arxiv.org/pdf/1409.1556
 class Discriminator(nn.Module): 
     def __init__(self): 
         super(Discriminator, self).__init__()
-        self.conv1x1 = nn.Conv2d(3, 64, kernel_size=(3, 3), padding=1)
-        self.conv1x2 = nn.Conv2d(64, 64, kernel_size=(3, 3), padding=1)
-        self.maxpooling1 = nn.MaxPool2d((2, 2), stride=2)
-
-        self.conv2x1 = nn.Conv2d(64, 128, kernel_size=(2,2), padding=1)
-        self.conv2x2 = nn.Conv2d(128, 128, kernel_size=(2,2))
-        self.maxpooling2 = nn.MaxPool2d((2,2), stride=2)
-
-        self.conv3x1 = nn.Conv2d(128, 256, (2,2), padding=1)
-        self.conv3x2 = nn.Conv2d(256, 256, (3, 3), padding=1)
-        self.conv3x3 = nn.Conv2d(256, 256, (3, 3), padding=1)
-        self.maxpooling3 = nn.MaxPool2d((2,2), stride=2)
-
-        self.conv4x1 = nn.Conv2d(256, 512, (3, 3), padding=1)
-        self.conv4x2 = nn.Conv2d(512, 512, (3, 3), padding=1)
-        self.conv4x3 = nn.Conv2d(512, 512, (3, 3), padding=1)
-        self.maxpooling4 = nn.MaxPool2d((2,2), stride=2)
-
-        self.conv5x1 = nn.Conv2d(512, 512, (3, 3), padding=1)
-        self.conv5x2 = nn.Conv2d(512, 512, (3, 3), padding=1)
-        self.conv5x3 = nn.Conv2d(512, 512, (3, 3), padding=1)
-        self.maxpooling5 = nn.MaxPool2d((2,2), stride=2)
-        
-        self.li1 = nn.Linear(25088, 4096)
-        self.li2 = nn.Linear(4096, 4096)
-        self.li3 = nn.Linear(4096, 1000)
+        pass 
 
     def forward(self, x): 
-        x = self.conv1x1(x)
-        x = self.conv1x2(x)
-        x = self.maxpooling1(x)
+        pass
 
-        x = self.conv2x1(x)
-        x = self.conv2x2(x)
-        x = self.maxpooling2(x)
-
-        x = self.conv3x1(x)
-        x = self.conv3x2(x)
-        x = self.conv3x3(x)
-        x = self.maxpooling3(x)
-
-        x = self.conv4x1(x)
-        x = self.conv4x2(x)
-        x = self.conv4x3(x)
-        x = self.maxpooling4(x)
-
-        x = self.conv5x1(x)
-        x = self.conv5x2(x)
-        x = self.conv5x3(x)
-        x = self.maxpooling5(x)
-
-        x = x.flatten(start_dim=1)
-
-        x = self.li1(x)
-        x = self.li2(x)
-        x = self.li3(x)
-
-        x = nn.Softmax(x)
-
-        return x
-
-
+# http://www.cs.toronto.edu/~rgrosse/courses/csc321_2018/assignments/a4-handout.pdf
 class Generator(nn.Module): 
     def __init__(self): 
         super(Generator, self).__init__()
+
+        # Encoding
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(3, 3), stride=2, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=(3, 3), stride=2, padding=1)
+
+        # Redidual Block
+
+        # Decoding
     
-    def forward(self): 
-        pass
+    def forward(self, x): 
+        x = self.conv1(x)
+        print(x.shape)
+
+        return x
