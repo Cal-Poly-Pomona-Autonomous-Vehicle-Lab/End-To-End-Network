8600c251db68bc4f7113b94254e325a25962d7fd
Conspiciens <fernandezjohann08@gmail.com>
2025-06-09 14:33:40 -0700

    updated bug fixes and started training

--- upgrade/dccgan.py
+++ upgrade/dccgan.py
@@ -4,35 +4,35 @@ import torch.nn.functional as F
 
 class Discriminator(nn.Module): 
     def __init__(self):
-        super(Generator, self).__init__()
-        self.conv1 = nn.Conv2d(64, 32, kernel_size=(3,3))
-        self.conv2 = nn.Conv2d(32, 16, kernel_size=(3,3))
-        self.conv3 = nn.Conv2d(16, 8, kernel_size=(3,3))
-        self.conv4 = nn.Conv2d(8, 4, kernel_size=(3,3))
-        self.conv5 = nn.Conv2d(4, 1, kernel_size=(3,3))
+        super(Discriminator, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(3,3))
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=(3,3))
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=(3,3))
+        self.conv4 = nn.Conv2d(128, 1, kernel_size=(3,3))
+        self.sig = nn.Sigmoid()
     
-    def foward(self, x): 
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
-
-        x = nn.Sigmoid(x)
-
-        return x
-
-class Generator(nn.Module): 
-    def __init__(self, noise_size, conv_dim): 
-        super(Discriminator, self).__init__() 
-        self.deconv1 = nn.ConvTranspose2d(1, 4, kernel_size=(3, 3))
-        self.deconv2 = nn.ConvTranspose2d(4, 8, kernel_size=(3, 3))
-        self.deconv3 = nn.ConvTranspose2d(16, 32, kernel_size=(3, 3))
-        self.deconv4 = nn.ConvTranspose2d(32, 64, kernel_size=(3, 3))
-
     def forward(self, x): 
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
         x = F.relu(self.conv4(x))
+
+        x = self.sig(x)
+
+        return x
+
+class Generator(nn.Module): 
+    def __init__(self, noise_size, conv_dim): 
+        super(Generator, self).__init__() 
+        self.deconv1 = nn.ConvTranspose2d(100, 128, kernel_size=(3, 3))
+        self.deconv2 = nn.ConvTranspose2d(128, 64, kernel_size=(3, 3))
+        self.deconv3 = nn.ConvTranspose2d(64, 32, kernel_size=(3, 3))
+        self.deconv4 = nn.ConvTranspose2d(32, 3, kernel_size=(3, 3))
+
+    def forward(self, x): 
+        x = F.relu(self.deconv1(x))
+        x = F.relu(self.deconv2(x))
+        x = F.relu(self.deconv3(x))
+        x = F.relu(self.deconv4(x))
 
         return x
--- upgrade/training_gan.py
+++ upgrade/training_gan.py
@@ -4,12 +4,24 @@ import torch.nn as nn
 import sys; sys.path.append('..')
 import preprocessing
 
+import matplotlib.pyplot as plt
+import matplotlib.image as mpng
+
 from torch.utils.data import DataLoader
-from gan import Generator
+from dccgan import Generator, Discriminator
 
 
 def training(): 
     EPOCH = 1
+
+    device = None
+    if torch.cuda.is_available(): 
+        device = 'cuda'
+    elif torch.backends.mps.is_available(): 
+        device = 'mps'
+    else:
+        device = 'cuda'
+    
 
     image_folder = "/Volumes/joeham/logging_camera_down/image_data/" 
     logging_folder = "/Volumes/joeham/logging_camera_down/logging_data/"
@@ -21,13 +33,78 @@ def training():
         merge_folder = "/media/jojo-main/joeham/logging_camera_down/"
 
     data = preprocessing.data_processing(image_folder, logging_folder, merge_folder)
-    train_dataloader = DataLoader(data, batch_size=10, shuffle=True)
+    train_dataloader = DataLoader(data, batch_size=1, shuffle=True)
+    
+    Dis = Discriminator() 
+    Gen = Generator(noise_size=100, conv_dim=3)
 
-    model = Generator() 
+    optimizer_D = torch.optim.Adam(Dis.parameters(), lr=0.0001) 
+    optimizer_G = torch.optim.Adam(Gen.parameters(), lr=0.0001) 
+
+    loss_fn = nn.MSELoss()
+    noise = torch.randn(1, 100, 1, 1)
+
+    img_count = 1 
+    total_img_count = len(data)
     for epoch in range(EPOCH): 
         for (front_img, steering) in train_dataloader: 
-            y = model(front_img)
-            break
+
+            if img_count > 1000: 
+                break
+            
+            pred_ds = Dis.forward(front_img)
+            fake_img = Gen.forward(noise)
+
+            target = torch.full_like(pred_ds, 1.0)
+            loss_real = loss_fn(pred_ds, target)
+
+            output_fake = Dis.forward(fake_img)
+            label_fake = torch.full_like(output_fake, 1)
+            loss_fake = loss_fn(output_fake, label_fake)
+
+            loss_D = 0.5 * (loss_real + loss_fake)
+
+            Dis.zero_grad() 
+            loss_D.backward() 
+            optimizer_D.step()
+
+            fake_output = Gen.forward(noise)
+
+            f_output = Dis.forward(fake_output)
+            label_gen = torch.full_like(f_output, 1)
+            loss_G = 0.5 * loss_fn(f_output, label_gen)
+
+            Gen.zero_grad() 
+            loss_G.backward() 
+            optimizer_G.step()
+
+            print(f"Loss Dis: {loss_D}")
+            print(f"Loss Gen: {loss_G}")
+            print(f"Progress: {img_count / total_img_count * 100:.2f}%")
+            img_count = img_count + 1
+
+
+    f_img = Gen.forward(noise)
+
+    f_img = f_img.detach().numpy() 
+    f_img = f_img.squeeze()
+    f_img = f_img.transpose(1, 2, 0)
+
+    plt.imshow(f_img)
+    plt.show()
+            
+def validation() -> None: 
+    image_folder = "/Volumes/joeham/logging_camera_down/image_data/" 
+    logging_folder = "/Volumes/joeham/logging_camera_down/logging_data/"
+    merge_folder = "/Volumes/joeham/logging_camera_down/"
+
+    if sys.platform == "linux": 
+        image_folder = "/media/jojo-main/joeham/logging_camera_down/image_data"
+        logging_folder = "/media/jojo-main/joeham/logging_camera_down/logging_data"
+        merge_folder = "/media/jojo-main/joeham/logging_camera_down/"
+            
+    data = preprocessing.data_processing(image_folder, logging_folder, merge_folder)
+    train_dataloader = DataLoader(data, batch_size=1, shuffle=True)
         
     
 
